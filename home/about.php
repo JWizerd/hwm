162245ef2d5881b170b2456bfc167de019074e01
JWizerd <jwodke@outlook.com>
2016-11-04 23:24:48 -0600

    added jquery media player and added to sermons

--- home/about.php
+++ home/about.php
@@ -14,7 +14,7 @@
         <h2><?php echo $about_title; ?></h2>
         <p><?php echo $about_description; ?></p>
         <div class="row">
-          <div class="general-info col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-4">
+          <div class="general-info col-xs-10 col-xs-offset-1 col-sm-4 col-sm-offset-4 shadow-text">
             <strong class="address"><?php echo $church_address; ?></strong>
             <strong class="services"><?php echo $service_times_title; ?></strong>
             <strong><?php echo $service_times; ?></strong>
--- home/sermons.php
+++ home/sermons.php
@@ -10,9 +10,10 @@
       <h2><?php echo $sermons_title; ?></h2>
       <h3><?php echo $sermons_call_to_action; ?></h3>
       <div class="col-md-10 col-md-offset-1">
-        <p class="rosey">
+        <p>
           <?php echo $sermons_listen_directions; ?>
         </p>
+        <?php echo do_shortcode('[playlist tracks="stuff.mp3, udulessig.mp3, Maria.mp3" style="nopopoutbutton"]'); ?>
       </div>
     </div>
   </div>
