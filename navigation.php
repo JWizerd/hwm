5dcdf9586ebefc5e39849280c1379192874a1ce3
JWizerd <jwodke@outlook.com>
2016-11-10 09:54:40 -0700

    fixed mobile responsiveness of the navigation menu

--- navigation.php
+++ navigation.php
@@ -1,16 +1,12 @@
 <?php
 $facebook_link = get_field('facebook_link');
 $navigation_link_title_1 = get_field('navigation_link_title_1');
-$navigation_link_1 = get_field('navigation_link_1');
 
 $navigation_link_title_2 = get_field('navigation_link_title_2');
-$navigation_link_2 = get_field('navigation_link_2');
 
 $navigation_link_title_3 = get_field('navigation_link_title_3');
-$navigation_link_3 = get_field('navigation_link_3');
 
 $navigation_link_title_4 = get_field('navigation_link_title_4');
-$navigation_link_4 = get_field('navigation_link_4');
 
 $navigation_link_title_5 = get_field('navigation_link_title_5');
 $navigation_link_5 = get_field('navigation_link_5');
@@ -19,17 +15,22 @@ $navigation_link_5 = get_field('navigation_link_5');
 <nav>
   <ul>
     <li>
-      <a href="<?php echo $navigation_link_1; ?>" class="shadow-text"><?php echo $navigation_link_title_1; ?></a>
+      <a href="#about" class="shadow-text"><?php echo $navigation_link_title_1; ?></a>
     </li>
     <li>
-      <a href="<?php echo $navigation_link_2; ?>" class="shadow-text"><?php echo $navigation_link_title_2; ?></a>
+      <a href="#events" class="shadow-text"><?php echo $navigation_link_title_2; ?></a>
     </li>
     <li>
-      <a href="<?php echo $navigation_link_3; ?>" class="shadow-text"><?php echo $navigation_link_title_3; ?></a>
+      <a href="#sermons" class="shadow-text"><?php echo $navigation_link_title_3; ?></a>
     </li>
     <li>
-      <a href="<?php echo $navigation_link_4; ?>" class="shadow-text"><?php echo $navigation_link_title_4; ?></a>
+      <a href="#contact" class="shadow-text"><?php echo $navigation_link_title_4; ?></a>
     </li>
+    <?php if (!empty($navigation_link_5)) : ?>
+      <li>
+        <a target="_blank" href="<?php echo $navigation_link_5; ?>" class="shadow-text"><?php echo $navigation_link_title_5; ?></a>
+      </li>
+    <?php endif; ?>
     <li>
       <a target="_blank" href="<?php echo $facebook_link; ?>" class="shadow-text"><i class="fa fa-facebook-square"></i> Facebook</a>
     </li>
